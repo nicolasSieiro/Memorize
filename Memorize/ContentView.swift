83ee82ca6d1abf88e0537b5f5dd5590e982bb429
nicolas sieiro <devhawk42@gmail.com>
2020-09-28 02:18:34 -0300

    Initial cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(0..<4) { index in
+                CardView()
+            }
+        }
+        .padding()
+        .foregroundColor(.orange)
+        .font(.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
 }
 
