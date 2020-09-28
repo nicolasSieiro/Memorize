f47e7e737f6fdc9008371caa63a3ff47cab2692e
nicolas sieiro <devhawk42@gmail.com>
2020-09-28 01:21:03 -0300

    Memorize setup

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Nicolas Sieiro on 28/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
